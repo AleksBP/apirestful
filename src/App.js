7c48b0f9b213b3d2b09f2c2e3cb135497bc46d3e
AleksBP <aleks.bep@hotmail.com>
2022-03-30 00:22:29 -0700

    [ADD] Instalación de Boostrap al proyecto.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './assets/css/App.css';
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <div className="App">
       <strong>Texto de prueba</strong>
+      <br/>
+      <button className='btn btn-primary'>Botón genérico</button>
     </div>
   );
 }
